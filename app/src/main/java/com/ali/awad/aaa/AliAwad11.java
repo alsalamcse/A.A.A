65adc4328d918aaecc2be4175a4c745059864503
Owner <ekrmaawad231@gmail.com>
2019-09-22 11:18:55 +0300

    Initial commit

---
+++ app/src/main/java/com/ali/awad/aaa/AliAwad11.java
@@ -0,0 +1,13 @@
+package com.ali.awad.aaa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AliAwad11 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ali_awad11);
+    }
+}
